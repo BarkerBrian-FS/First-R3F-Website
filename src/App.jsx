7d8fb4272d243a87a7f9e019b7c26b2bf32ff08b
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-05-12 12:39:02 -0500

    added 2 more cubes and fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
-
 import './App.css'
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei'
 
+function Box({ position }) {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
 
-  function Box(){
-    const ref = useRef()
-    const [hovered, setHovered] = useState(false)
-   
+  useFrame(({ clock }) => {
+    if (!ref.current) return
 
-    useFrame(({clock}) => {
-      ref.current.position.y += Math.sin(clock.elapsedTime) * (hovered ? 0.8 : 0.3)
-      ref.current.rotation.y += hovered ? 0.03: 0.01
-    })
-    return (
-      <mesh ref={(ref)}>
-        <boxGeometry args={[2,2,2]} />
-        <meshStandardMaterial color='hotpink' />
-      </mesh>
-    )
-  }
+    ref.current.position.y =
+      position[1] + Math.sin(clock.elapsedTime) * (hovered ? 0.3 : 0.1)
+
+    ref.current.rotation.y += hovered ? 0.03 : 0.01
+  })
+
+  return (
+    <mesh
+      ref={ref}
+      position={position}
+      onPointerEnter={() => setHovered(true)}
+      onPointerLeave={() => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'orange' : 'hotpink'} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <>
-      <div>
-        <Canvas>
-          <ambientLight intensity={.05} />
-          <directionalLight position={[3,3,3]} intensity={1} />
-          <Box />
-          <OrbitControls enableDamping />
-        </Canvas>
-      </div>
-    </>
+    <Canvas style={{ width: '100vw', height: '100vh' }}>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 5]} intensity={1.5} />
+      <pointLight position={[-5, -5, -5]} intensity={0.5} />
+
+      <Box position={[-2, 0, 0]} />
+      <Box position={[0, 0, 0]} />
+      <Box position={[2, 0, 0]} />
+
+      <OrbitControls enableDamping />
+    </Canvas>
   )
 }
 
