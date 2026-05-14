17ac236a4ebf96230b02e7e467df47a1ef7bcb37
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-05-14 10:56:51 -0500

    updated html section app and scene

--- src/Components/HtmlSection.jsx
+++ src/Components/HtmlSection.jsx
@@ -3,9 +3,21 @@ import {Html} from '@react-three/drei'
 const HtmlSection = () => {
   return (
     <Html fullscreen>
-        <div className='hero'>
-            <h1>Welcome to Space</h1>
-            <p>Scroll to explore the universe</p>
+        <div className='sections'>
+            <section className ='panel'>
+                <h1>Welcome to Space</h1>
+                <p>A Cinematic React Three Fiber experience</p>
+            </section>
+
+            <section className ='panel'>
+                <h1>Floating Through</h1>
+                <p>Object drift as the camera moves</p>
+            </section>
+
+            <section className ='panel'>
+                <h1>Core Focus</h1>
+                <p>The journey ends at the focal object</p>
+            </section>
         </div>
     </Html>
   )
--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -1,38 +1,36 @@
 import { Stars } from '@react-three/drei'
 import Box from './Box'
-import CameraRig from './Components/CameraRig'
-import FocalObject from './Components/FocalObject'
+import FocalObject from './FocalObject'
 
 
 const Scene = () => {
 
   return (
-    <div>
-        <CameraRig />
+    <>
         
-                <color attach="background" args={['#050816']} />
+      <color attach="background" args={['#050816']} />
         
-                <Stars
-                  radius={300}
-                  depth={80}
-                  count={8000}
-                  factor={6}
-                  saturation={0}
-                  fade
-                />
+      <Stars
+        radius={300}
+        depth={80}
+        count={8000}
+        factor={6}
+        saturation={0}
+        fade
+      />
         
-                <fog attach="fog" args={['#070b1a', 10, 30]} />
+      <fog attach="fog" args={['#070b1a', 10, 30]} />
         
-                <ambientLight intensity={0.25} color='#6a7cff' />
-                <directionalLight position={[5, 5, 5]} intensity={1} />
-                <pointLight position={[-5, -5, -5]} intensity={1.5} color='#ff4fd8' />
+      <ambientLight intensity={0.25} color='#6a7cff' />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <pointLight position={[-5, -5, -5]} intensity={1.5} color='#ff4fd8' />
         
-                <FocalObject />
+      <FocalObject />
         
-                <Box position={[-3, 1, -1]} />
-                <Box position={[3, -1, 1]} />
-                <Box position={[0, 2, -2]} />
-    </div>
+      <Box position={[-3, 1, -1]} />
+      <Box position={[3, -1, 1]} />
+      <Box position={[0, 2, -2]} />
+    </>
     
   )
 }
